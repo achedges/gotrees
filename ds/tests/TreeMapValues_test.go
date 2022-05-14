6897d1d68abe7cdeaaa83e1b9628cbbe3e7d2745
Adam Hedges <achedges@gmail.com>
2022-05-13 20:32:07 -0500

    Added unit test on key/value pairs

--- ds/tests/TreeMapValues_test.go
+++ ds/tests/TreeMapValues_test.go
@@ -5,6 +5,11 @@ import (
 	"testing"
 )
 
+func fail(t *testing.T, node int) {
+	t.Errorf("Incorrect value at node %d", node)
+	t.FailNow()
+}
+
 func TestTreeMapKeyValuePairs(t *testing.T) {
 	tree := ds.NewTree[int]()
 	ds.AddKeyValue(tree, 1, "A")
@@ -17,5 +22,23 @@ func TestTreeMapKeyValuePairs(t *testing.T) {
 		t.FailNow()
 	}
 
-	//node1 := tree.Find(1)
+	node1 := tree.Find(1).(*ds.KeyValueNode[int, string])
+	if node1.GetValue() != "A" {
+		fail(t, 1)
+	}
+
+	node2 := tree.Find(2).(*ds.KeyValueNode[int, string])
+	if node2.GetValue() != "B" {
+		fail(t, 2)
+	}
+
+	node3 := tree.Find(3).(*ds.KeyValueNode[int, string])
+	if node3.GetValue() != "C" {
+		fail(t, 3)
+	}
+
+	node4 := tree.Find(4).(*ds.KeyValueNode[int, string])
+	if node4.GetValue() != "D" {
+		fail(t, 4)
+	}
 }
